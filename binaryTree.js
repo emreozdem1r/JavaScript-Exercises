ebcc03c09bc3ce913acedb6dce131582ee3d3cbf
emreozdem1r <emreozdemir3536@gmail.com>
2019-03-12 18:57:31 +0300

    tree has been updated.

--- binaryTree.js
+++ binaryTree.js
@@ -57,15 +57,45 @@ class BinarySearchTree{
 	getRootNode(){ 
 		    return this.root; 
 	} 
+	findMinNode(node){
+	if(node.left===null)
+			return node;
+	else
+			return this.findMinNode(node.left); 
+	}
+	findMaxNode(node){
+	if(node.right===null)
+			return node;
+	else
+			return this.findMaxNode(node.right); 
+	}
+	search(node, data) 
+	{
+		if(node.data===data)
+			return data;
+		else if(node.data<data)
+			return this.search(node.right,data);
+		
+		else if(node.data>data)
+			return this.search(node.left,data);
+		
+		else 
+			return node.data;
+	}
 }
 var tree = new BinarySearchTree();
-	tree.insert(1);
-	tree.insert(3);
 	tree.insert(5);
+	tree.insert(3);
+	tree.insert(1);
 	tree.insert(2);
 	tree.insert(7);
 	tree.insert(4);
 	tree.insert(6);
 
-var root=tree.getRootNode();
-tree.inorder(root);
+	var root=tree.getRootNode();
+	tree.inorder(root);
+	console.log("min node: " +tree.findMinNode(root).data);
+	console.log("max node: " +tree.findMaxNode(root).data);
+	console.log("find searchin data: " +tree.search(root,4));
+
+
